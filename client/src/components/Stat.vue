4cc9678ccc21c97786f41b8299d700ff7252937d
antaeseon <ridgnfu@naver.com>
2018-11-26 15:34:17 +0900

    stat vue design clear

--- client/src/components/Stat.vue
+++ client/src/components/Stat.vue
@@ -9,17 +9,17 @@
         </div>
         <br><br> 
         <div class="border border-secondary mt-5">
-            <b-row class= m-1>
+            <b-row class= "m-1">
                 <b-col cols="4" class="ml-10">
                         <b-img left src="https://picsum.photos/125/125/?image=43" rounded="circle" height='70%' width='70%'  class="mr-3 mt-1"/>
                         <p class="text-left mt-4">{{this.$store.getters.id}}</p>
                 </b-col>
                 <b-col cols="8">
                     <b-row class="border border-info w-50 ">
-                        <b-col cols="5">
+                        <b-col cols="5" class="bg-primary text-white">
                             <p class="text-left ">Win : 8</p>
                         </b-col>
-                        <b-col cols="5">
+                        <b-col cols="5" class="bg-dark text-white">
                             <p class="text-right">Lose : 9</p>
                         </b-col>
                         <b-col cols="2" class="border black">
@@ -39,6 +39,11 @@
                 </b-col>
             </b-row>
         </div>
+        <hr>
+        <ul class="list-group w-50 mx-auto">
+            <li class="list-group-item list-group-item-info" v-for="item in items">{{item}}</li>
+        </ul>
+
     </div>            
 </template>
 
@@ -47,7 +52,7 @@ import mercenary from './modal/mercenary'
 export default {
     data(){
         return{
-
+            items:["dd","aa"]
         }
     },
     components:{
