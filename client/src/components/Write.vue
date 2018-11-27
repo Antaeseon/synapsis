acce9a238b831c001d78fa9543593f36ff9e451d
songhansol <dkwlrjf@ajou.ac.kr>
2018-11-27 21:22:03 +0900

    게시판 수정

--- client/src/components/Write.vue
+++ client/src/components/Write.vue
@@ -1,23 +1,23 @@
 <template>
   <div id='write'>
     <h4>새로운 게시글 작성하기</h4>
-      <b-container textarea1>
-      <b-row class="my-1">
-        <b-col sm="2"><label for="post-title">제목:</label></b-col>
-        <b-col sm="8">
-          <b-form-input id="post-title" type="text" placeholder="Enter title"></b-form-input>
+      <b-container class = "textarea1">
+      <b-row align-h="center">
+        <b-col sm="1"><label for="post-title">제목:</label></b-col>
+        <b-col sm="9">
+          <b-form-input id="post-title" v-model="title" type="text" placeholder="Enter title"></b-form-input>
         </b-col>
       </b-row>
     </b-container>
-    <b-container textarea2>
-      <b-row class="my-2">
-        <b-col sm="2"><label for="post-text">내용:</label></b-col>
-        <b-col sm="8">
-        <b-form-textarea id="post-text" v-model="text" placeholder="Enter context" :rows="3" :max-rows="6"></b-form-textarea>
+    <b-container class = "textarea2">
+      <b-row align-h="center">
+        <b-col sm="1"><label for="post-text">내용:</label></b-col>
+        <b-col sm="9">
+        <b-form-textarea id="post-text" v-model="context" placeholder="Enter context" :rows="3" :max-rows="10"></b-form-textarea>
         </b-col>
       </b-row>
     </b-container>
-    <b-button>등록</b-button>
+    <b-button id="submit">등록</b-button>
   </div>
 </template>
 
@@ -25,7 +25,8 @@
 export default {
   data () {
     return {
-      text: ''
+      title: '',
+      context: ''
     }
   },
   methods: {
@@ -40,9 +41,12 @@ h4{
   margin: 20px;
 }
 .textarea1{
-  margin: 10px;
+  margin-top: 50px;
 }
 .textarea2{
-  margin: 10px;
+  margin-top: 20px;
+}
+#submit{
+  margin-top: 20px;
 }
 </style>
