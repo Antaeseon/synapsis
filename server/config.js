1f86f26e842bd56990a106e9ad4c7fa3f059a073
imseungbae <tmdqo2013@naver.com>
2018-11-30 11:48:31 +0900

    cloud DB

--- server/config.js
+++ server/config.js
@@ -2,4 +2,5 @@
 module.exports = {
     'secret' : 'taeseon',
     'mongodbUri' : 'mongodb://localhost:27017/synap'
+    //'mongodbUri' : 'mongodb+srv://tmdqo9:wndrnr12@cluster0-mj7yp.mongodb.net/synap'
 }
