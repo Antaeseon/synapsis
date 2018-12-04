0846d222e7134cdc53a9011c31fafb1438e3db96
imseungbae <tmdqo2013@naver.com>
2018-12-04 19:02:21 +0900

    Third ranking + posting

--- server/routes/posting.js
+++ server/routes/posting.js
@@ -6,7 +6,7 @@ const Post = require("../models/post");
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-// 게시글 목록 받아오기
+// 게시글 전체 타이틀 목록 받아오기
 router.get("/", function(req, res) {
   Post.find({}, function(err, result) {
     if (err) return res.status(500).send({ error: "DB is not found!" });
@@ -23,13 +23,31 @@ router.post("/register", function(req, res) {
   post.user_id = req.body.user_id;
   post.date = Date.now();
 
+  //카운트,인덱스는 디폴트 설정값이 있음.
   post.save(function(err) {
     if (err) {
-      console.error("error");
+      console.error(err);
       return;
     }
     res.send({ result: 1 });
   });
 });
 
+// 게시글 상세 보기.
+router.post("/:index", function(req, res) {
+   var idx = req.params.index;
+
+   // 해당 게시글의 조회수 증가.
+   Post.update({index:idx},{$inc: {count : 1}},function(err,result){ 
+    if (err) return res.status(500).send({ error: "조회수 업데이트 실패." });
+    console.log("조회수 업데이트 성공!");
+    });
+
+   // 인덱스에 해당하는 게시글 불러오기.
+   Post.find({index:idx}, function(err, result) {
+    if (err) return res.status(500).send({ error: "해당 글이 없습니다." });
+    res.json(result);
+    });
+
+}); 
 module.exports = router;
