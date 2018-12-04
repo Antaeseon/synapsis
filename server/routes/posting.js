d9152ea7e01b15ff87cabb4857a8c989e6070309
imseungbae <tmdqo2013@naver.com>
2018-12-04 18:33:44 +0900

    first ranking + posting

--- server/routes/posting.js
+++ server/routes/posting.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 const Post = require("../models/post");
 
-router.get("/", function(req, res){
-    Post.find({}, function(err, result){
-        if(err) return res.status(500).send({ error : "DB is not found!"});
-        res.json(result);
-        console.log(result);
-    }).setOptions({score: -1});
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+// 게시글 목록 받아오기
+router.get("/", function(req, res) {
+  Post.find({}, function(err, result) {
+    if (err) return res.status(500).send({ error: "DB is not found!" });
+    res.json(result);
+    console.log(result);
+  }).setOptions({ score: -1 });
+});
+
+// 게시글 등록하기.
+router.post("/register", function(req, res) {
+  var post = new Post();
+  post.title = req.body.title;
+  post.context = req.body.content;
+  post.user_id = req.body.user_id;
+
+  post.save(function(err) {
+    if (err) {
+      console.error("error");
+      return;
+    }
+    res.send({ result: 1 });
+  });
 });
 
 module.exports = router;
