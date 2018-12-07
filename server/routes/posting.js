9357905a40c63698f338e3d74022d594106fb78e
imseungbae <tmdqo2013@naver.com>
2018-12-07 13:09:24 +0900

    rank+post error handling

--- server/routes/posting.js
+++ server/routes/posting.js
@@ -22,30 +22,33 @@ router.post("/register", function(req, res) {
   post.date = Date.now();
 
   //카운트,인덱스는 디폴트 설정값이 있음.
-  post.save(function(err) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    res.send({ result: 1 });
-  });
+  if (!post.user_id) res.json({ result: 0 });
+  else {
+    post.save(function(err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.json({ result: 1 });
+    });
+  }
 });
 
 // 게시글 상세 보기.
 router.get("/:index", function(req, res) {
-   var idx = req.params.index;
-   
-   // 해당 게시글의 조회수 증가.
-   Post.update({index:idx},{$inc: {count : 1}},function(err,result){ 
+  var idx = req.params.index;
+
+  // 해당 게시글의 조회수 증가.
+  Post.update({ index: idx }, { $inc: { count: 1 } }, function(err, result) {
     if (err) return res.status(500).send({ error: "조회수 업데이트 실패." });
     console.log("조회수 업데이트 성공!");
-   });
+  });
 
-   // 인덱스에 해당하는 게시글 불러오기.
-   Post.find({index:idx}, function(err, result) {
+  // 인덱스에 해당하는 게시글 불러오기.
+  Post.find({ index: idx }, function(err, result) {
     if (err) return res.status(500).send({ error: "해당 글이 없습니다." });
     console.log(result);
     res.json(result);
-   });
-}); 
+  });
+});
 module.exports = router;
