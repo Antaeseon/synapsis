f354525ea404e76ac490b244554235aa9a3acd92
songhansol <dkwlrjf@ajou.ac.kr>
2018-12-09 12:01:37 +0900

    용병게시판 프론트 함수 구현

--- server/models/person.js
+++ server/models/person.js
@@ -1,37 +1,55 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+//Auto Increment 플러그인
+const autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(mongoose.connection)
+
 const Person = new Schema({
     index:{
         type: Number,
         unique: true
     },
-    user_name:{type:String},
+    user_id:{type:String},
     region:{type:String},
-    
+    category:{type:String},
+    position:{type:String},
+    date:{type: Date},
+    time:{type:String},
+    selected:{type:boolean}
 })
 
-Person.statics.create = function(id, sportsCategory, position, region) {
+//인덱스 자동으로 1씩 증가되도록
+Person.plugin(autoIncrement.plugin,{
+    model: "Person",
+    field: "index",
+    startAt: 1
+});
+
+Person.statics.create = function(user_id, region, category, position, date, time, selected) {
     const user = new this({
-        id,
-        sportsCategory,
+        user_id,
+        region,
+        category,
         position,
-        region
+        date,
+        time,
+        selected
     })
 
     // return the Promise
     return user.save()
 }
 
-Person.statics.findOneById = function(id) {
+Person.statics.findOneById = function(user_id) {
     return this.findOne({
-        id
-    }).exec()
+        user_id
+    })
   }
   
 
 //해당 id의 용병체크를 변경하는 함수
-Person.methods.personselected = function(id){
+Person.methods.personselected = function(user_id){
     this.isChecked = true
 }
 
